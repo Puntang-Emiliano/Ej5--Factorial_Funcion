9acab506e03bd90531c6d828b4a0271b9f32fd4b
Puntang Emiliano <Puntang.emiliano@gmail.com>
2022-10-11 22:08:24 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Ej5__Factorial_Funcion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.ForegroundColor= ConsoleColor.Magenta;
+           
+           
+
+            Console.WriteLine("          ******* BIENBENIDO AL SISTEMA DE FACTORIALES *******"          );
+            Console.WriteLine("LA FACTORIAL DEL NUMERO INGRESADO ES: {0}", factorial());
+            Console.ReadKey();
+            
+
+           
+            
+            
+
+        }
+
+        
+        static int factorial()
+        {
+
+            int acumulador, contador,fac ;
+            acumulador=1;
+            Console.WriteLine("INGRESE EL NUMERO DEL CUAL QUIERE CALCULAR LA FACTORIAL");
+            fac= Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            
+            for (contador =1; contador <= fac; contador++)
+            {
+                acumulador = acumulador * contador;
+            }
+            return acumulador;
+        }
+
+        
+
+
+
+    }
+}
